dceb69d2f8dd99d9f513f6172269a2eda74b69c0
fscelliott <42477011+fscelliott@users.noreply.github.com>
2019-11-10 22:39:59 -0700

    project: functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,38 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 var cardOne = cards[0];
 var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length == 2) {
-   if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-   }
-   else {
-        alert("sorry, try again");
-   }
-        
-   
+
+
+
+
+
+function flipCard(cardId) {
+    console.log( "User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length == 2) {
+       checkForMatch();
+       }
+      
     
+
 }
+
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+
+
+flipCard(2);
+flipCard(0);
+
+
+
 
 console.log("User flipped " + cardsInPlay[cardsInPlay.length-2]);
 console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
