ca615a3945f1374c033b0cc9b15f680fd40d53e4
fscelliott <42477011+fscelliott@users.noreply.github.com>
2019-11-10 22:28:35 -0700

    control flow project

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,22 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length == 2) {
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+   }
+   else {
+        alert("sorry, try again");
+   }
+        
+   
+    
+}
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-2]);
+console.log("User flipped " + cardsInPlay[cardsInPlay.length-1]);
